3dd4550e4f2cf10100800e8f3efb0bf69c9917ae
Sergio Berlotto <sergio-berlotto@gg.rs.gov.br>
2012-08-10 18:17:28 -0300

    Menu de admin, e itens novos na home

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template, abort
 
-blog = Blueprint('blog', __name__,template_folder='templates', static_folder='static')
+blog = Blueprint('blog', __name__, template_folder='templates', static_folder='static')
 
 @blog.route('/')
 def blog_index():
     return render_template('index.html')
+
+@blog.route('/new-post')
+def new_post():
+	return render_template('new-post.html')
