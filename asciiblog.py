8d5846718f83ad500fc65daa35ae30a2c35cec48
Sergio Berlotto <sergio.berlotto@gmail.com>
2012-08-16 00:40:32 -0300

    Twitter, FLickr vis RSS, Posts e Comentarios na CAPA

--- asciiblog.py
+++ asciiblog.py
@@ -1,14 +1,23 @@
 # -*- encoding: utf-8 -*-
 
 from flask import Flask, render_template, request, jsonify
+from database import db_session
 import markdown
+from json import loads
+from urllib import urlopen
+import flickrapi
+import feedparser
 
 #Blueprint Applications
 from blog import blog
+from blog.models import Post, Comment
 
 #========================================= CONFIGURATION
 app = Flask(__name__)
 app.config.from_pyfile('asciiblog.cfg')
+
+TWITTER_JSON_TIMELINE_URL = \
+	"http://api.twitter.com/1/statuses/user_timeline.json?screen_name=berlottocdd&include_rts=true&count=5"
 
 #========================================= UPLOADS
 from flaskext.uploads import UploadSet, configure_uploads, IMAGES, UploadNotAllowed
@@ -34,10 +43,27 @@ def translate_markdown(env, texto):
 		html = Markup(html)
 	return html
 
+#========================================= UTIL METHODS
+def get_twitts():
+	json = urlopen(TWITTER_JSON_TIMELINE_URL).read()
+	message = loads(json)
+	return message
+
+
 #========================================= MAIN ROUTES
 @app.route('/')
 def index():
-    return render_template('home.html')
+	#get last tweets
+	twitts = get_twitts()
+	#get last posts
+	posts = db_session.query(Post).order_by("date_created desc").limit(10)
+	#get last comments
+	comments = db_session.query(Comment).order_by("date_created desc").limit(10)
+	#last photo of flickr from RSS
+	d = feedparser.parse('http://api.flickr.com/services/feeds/photos_public.gne?id=51882400@N00&lang=pt-br&format=rss_200')
+	#last_photo = d['entries'][0]['media_thumbnail'][0]['url']
+	last_photo = d['entries'][0]['summary']
+	return render_template('home.html', posts=posts, comments=comments, twitts=twitts, flickr_photo=last_photo)
 
 @app.route('/slugfy/')
 def slugfy():
